78f601212725a6a1f832316c5369c88b2b5d7595
平兄 <597914752@qq.com>
2018-09-20 14:42:17 +0800

    完成卡片样式

---
+++ index.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: https://pingxonline.com/
+ * Date: 2018-09-20
+ * Time: 11:50
+ */
+
+?>
+
+
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.bootcss.com/amazeui/2.7.2/css/amazeui.min.css" rel="stylesheet">
+    <title>最美军训服照片</title>
+    <!-- Set render engine for 360 browser -->
+    <meta name="renderer" content="webkit">
+
+    <!-- No Baidu Siteapp-->
+    <meta http-equiv="Cache-Control" content="no-siteapp"/>
+    <link rel="stylesheet" href="css/index.css">
+</head>
+<body>
+
+    <header>
+
+    </header>
+    <main>
+        <div class="card">
+            <div class="card-img">
+                <img src="images/test.jpg" alt="图片">
+                <p class="card-img-hot"><i class="am-icon-eye"></i> 人气：</p>
+            </div>
+            <div class="card-desc">
+                <p class="card-name">1. 小明</p>
+                <p class="card-say">测试测试测试测试测试</p>
+            </div>
+            <div class="card-btn-group">
+                <div class="am-btn-group am-btn-group-justify">
+                    <a class="am-btn am-btn-danger" role="button"><i class="am-icon-check-square-o"></i>投票</a>
+                    <a class="am-btn am-btn-primary" role="button"><i class="am-icon-th-list"></i>评论</a>
+                    <a class="am-btn am-btn-success" role="button"><i class="am-icon-share-alt"></i>分享</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+
+    <script src="https://cdn.bootcss.com/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdn.bootcss.com/amazeui/2.7.2/js/amazeui.min.js"></script>
+    <script src="js/lib/template-web.js"></script>
+</body>
+</html>
